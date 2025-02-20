0f8697cd42b0b8f7a4fd07d01e892db6690606b4
JulianK75 <julian.kummer@hsbi.de>
2025-02-20 15:02:15 +0100

    Verbesserung Client und Server

--- server.C
+++ server.C
@@ -21,7 +21,8 @@
 #include <fstream>
 
 
-class KartServer : public TCPserver{
+class KartServer : public TCPserver
+{
 public:
     KartServer(int port, int bufferSize);
 
@@ -31,50 +32,89 @@ protected:
     bool toController(int x, int y, int p);
 };
 
+bool lessThanFour(int input)
+{
+    string s;
 
-int isFour(int input){
-    int length = to_string(input).length();
+    s = to_string(input);
+    while(s.length() < 4){
+        s.insert (0, 1, '0');
+    }
+    if(s.length() <= 4)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-    return length;
+bool isBool(int input)
+{
+    if(input == 0 || input == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 
 int main()
 {
-	srand(time(nullptr));
+    srand(time(nullptr));
     KartServer srv(2023,25);
     srv.run();
 
-	return 0;
+    return 0;
 }
 
+string toFour(int input){
+    string output;
 
-KartServer::KartServer(int port, int bufferSize) : TCPserver(port, bufferSize){
+    output = to_string(input);
+    while(output.length() < 4){
+        output.insert (0, 1, '0');
+    }
+    return output;
+}
+
+KartServer::KartServer(int port, int bufferSize) : TCPserver(port, bufferSize)
+{
 
 }
 
-string KartServer::myResponse(string input){
+string KartServer::myResponse(string input)
+{
 
     stringstream o;
     int x, y, e, p, r;
     bool coordFlag, penFlag, control;
 
 
-    if (input.compare(0,5,"start") == 0){
+    if (input.compare(0,5,"start") == 0)
+    {
         return string ("Warten auf Koordinaten(COORD[x,y]). \n");
     }
 
-    if (input.compare(0,6,"COORD[") == 0){
-        if (coordFlag==1){
+    if (input.compare(0,6,"COORD[") == 0)
+    {
+        if (coordFlag==1)
+        {
             return string ("Koordinaten wurden bereits eingegeben! Wenn Sie die Koordinaten zurücknehmen oder überschreiben wollen, nutzen Sie 'Back', bis die Koordinaten gelöscht werden. \n");
-            }
+        }
         e=sscanf(input.c_str(), "COORD[%i,%i]",&x, &y);
-        if (e!= 2){
+        if (e!= 2)
+        {
             return string ("COULD NOT READ COORDINATES \n");
         }
         coordFlag=true;
 
-        if(coordFlag==true && penFlag==true){
+        if(coordFlag==true && penFlag==true)
+        {
             o << "x" << x << "y" << y << "p" << p << "r1e" << ", zum Bestätigen 'y', zur Bearbeitung 'back'. \n";
             return (o.str());
         }
@@ -84,19 +124,23 @@ string KartServer::myResponse(string input){
 
     }
 
-    if (input.compare(0,4,"PEN[") == 0){
-        if (penFlag==1){
+    if (input.compare(0,4,"PEN[") == 0)
+    {
+        if (penFlag==1)
+        {
             return string ("Stiftposition wurde bereits eingegeben! Wenn Sie die Stiftposition zurücknehmen oder überschreiben wollen, nutzen Sie 'back', bis die Stiftposition gelöscht wird. \n");
-            }
+        }
         sscanf(input.c_str(), "PEN[%i]", &p);
-        if(p!=0&&p!=1){
+        if(p!=0&&p!=1)
+        {
             p=0;
             return string ("Ungültige Stiftposition! Erlaubt ist 0 (oben) oder 1 (unten)! \n");
         }
         penFlag=true;
 
-        if(coordFlag==true && penFlag==true){
-           o << "x" << x << "y" << y << "p" << p << "r1e" << ", zum Bestätigen 'y', zur Bearbeitung 'back'. \n";
+        if(coordFlag==true && penFlag==true)
+        {
+            o << "x" << x << "y" << y << "p" << p << "r1e" << ", zum Bestätigen 'y', zur Bearbeitung 'back'. \n";
             return (o.str());
         }
 
@@ -105,16 +149,23 @@ string KartServer::myResponse(string input){
 
     }
 
-    if (input.compare(0,1,"x") == 0){
-        sscanf(input.c_str(), "x%i""y%i""p%i""r%i""e",&x,&y,&p,&r);
-        if(isFour(x) < 4 || isFour(x) > 4){
+    if (input.compare(0,1,"x") == 0)
+    {
+        sscanf(input.c_str(), "x%d""y%d""p%i""r%i""e",&x,&y,&p,&r);
+        if(lessThanFour(x) == false)
+        {
             return string ("X-Coordinate is too small! 4 Digits required! \n");
         }
-        if(isFour(y) < 4 || isFour(y) > 4){
+        if(lessThanFour(y) == false)
+        {
             return string ("Y-Coordinate is too small! 4 Digits required! \n");
         }
+        if(isBool(p) == false)
+        {
+            return string ("Invalid pen position! Allowed are 0 (up) or 1 (down). \n");
+        }
 
-        o << "x" << x << "y" << y << "p" << p << "r" << r << "e gespeichert \n";
+        o << "x" << toFour(x) << "y" << toFour(y) << "p" << p << "r" << r << "e gespeichert \n";
 
         //Weiterleitung an Mikrocontroller
         toController(x,y,p);
@@ -122,36 +173,53 @@ string KartServer::myResponse(string input){
         return (o.str());
     }
 
-    if (input.compare(0,4,"back") == 0){
-        if(coordFlag==1 && penFlag==1){
+    if (input.compare(0,4,"back") == 0)
+    {
+        if(coordFlag==1 && penFlag==1)
+        {
             penFlag=0;
             p=0;
             return string ("Stiftposition gelöscht. Neue Eingabe möglich. \n");
-        }else if(coordFlag==1 && penFlag==0){
+        }
+        else if(coordFlag==1 && penFlag==0)
+        {
             coordFlag=0;
             x=0;
             y=0;
             return string ("Koordinaten gelöscht. Neue Eingabe möglich. \n");
-        }else{
+        }
+        else
+        {
             return string ("Keine Eingaben zum Löschen gefunden. Warten auf Koordinaten(COORD[x,y]). \n");
         }
     }
 
-    if (input.compare(0,1,"y") == 0){
-        if(coordFlag==0 && penFlag==0){
+    if (input.compare(0,1,"y") == 0)
+    {
+        if(coordFlag==0 && penFlag==0)
+        {
             return string ("Keine Koordinaten und Stiftposition zum Senden. \n");
-        }else if(coordFlag==1 && penFlag==0){
+        }
+        else if(coordFlag==1 && penFlag==0)
+        {
             return string ("Keine Stiftposition zum Speichern. \n");
-        }else if(coordFlag==0 && penFlag==1){
+        }
+        else if(coordFlag==0 && penFlag==1)
+        {
             return string ("Keine Koordinaten zum Speichern. \n");
-        }else{
+        }
+        else
+        {
             control=toController(x,y,p);
-            if(control=true){
+            if(control=true)
+            {
                 o << "x" << x << "y" << y << "p" << p << "r1e" << " erfolgreich gespeichert. \n";
                 coordFlag=false;
                 penFlag=false;
                 return (o.str());
-            }else if(control=false){
+            }
+            else if(control=false)
+            {
                 return string ("Fehler beim Öffnen der Datei! \n");
             }
 
@@ -162,18 +230,22 @@ string KartServer::myResponse(string input){
     return string ("ERROR \n");
 }
 
-bool KartServer::toController(int x, int y, int p){
+bool KartServer::toController(int x, int y, int p)
+{
     //Weiterleitung an Mikrocontroller
     std::ofstream file("../Projekt_XY_Plotter/Eckpunkte.txt", std::ios_base::app);  // Öffnen der Textdatei zum Schreiben
-        if (file.is_open()) {
-            file << "x" << x << "y" << y << "p" << p << "r1e" << "\n";
-            file.close();  // Datei schließen
-            std::cout << "Eckpunkte in 'eckpunkte.txt' gespeichert! \n" << std::endl;
-            return true;
-        } else {
-            std::cerr << "Fehler beim Öffnen der Datei! \n" << std::endl;
-            return false;
-        }
+    if (file.is_open())
+    {
+        file << "x" << toFour(x) << "y" << toFour(y) << "p" << p << "r1e" << "\n";
+        file.close();  // Datei schließen
+        std::cout << "Eckpunkte in 'eckpunkte.txt' gespeichert! \n" << std::endl;
+        return true;
+    }
+    else
+    {
+        std::cerr << "Fehler beim Öffnen der Datei! \n" << std::endl;
+        return false;
+    }
 
 }
 
